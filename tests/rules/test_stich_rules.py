af0f159102e91c55038724461a5972338c1f9532
Murthy10 <samuel.kurath@gmail.com>
2018-03-28 10:05:26 +0200

    added test_stich

--- tests/rules/test_stich_rules.py
+++ tests/rules/test_stich_rules.py
@@ -1,11 +1,24 @@
 import pytest
 
-@pytest.mark.parametrize("played_cards", [
-    (Trumpf.OBE_ABE, 2),
-    (Trumpf.UNDE_UFE, 3),
-    (Trumpf.BELL, 3),
-    (Trumpf.ACORN, 1),
+from pyMiniJass.deck import Card
+from pyMiniJass.game import PlayedCard
+from pyMiniJass.player.random_player import RandomPlayer
+from pyMiniJass.suit import Suit
+from pyMiniJass.rules.stich_rules import stich_rule
+
+
+@pytest.mark.parametrize("played_cards, winner", [
+    ([PlayedCard(player=RandomPlayer(name='a'), card=Card(value=8, suit=Suit.A)),
+      PlayedCard(player=RandomPlayer(name='b'), card=Card(value=7, suit=Suit.A))], 'a'),
+    ([PlayedCard(player=RandomPlayer(name='a'), card=Card(value=1, suit=Suit.A)),
+      PlayedCard(player=RandomPlayer(name='b'), card=Card(value=8, suit=Suit.B)),
+      PlayedCard(player=RandomPlayer(name='c'), card=Card(value=1, suit=Suit.B)),
+      PlayedCard(player=RandomPlayer(name='d'), card=Card(value=7, suit=Suit.A))], 'd'),
+    ([PlayedCard(player=RandomPlayer(name='a'), card=Card(value=1, suit=Suit.B)),
+      PlayedCard(player=RandomPlayer(name='b'), card=Card(value=8, suit=Suit.B)),
+      PlayedCard(player=RandomPlayer(name='c'), card=Card(value=1, suit=Suit.B)),
+      PlayedCard(player=RandomPlayer(name='d'), card=Card(value=7, suit=Suit.B))], 'b')
 ])
-def test_stich(played_cards):
-    stich = stich_rules[trumpf](played_cards=played_cards)
-    assert stich.player is players[index]
+def test_stich(played_cards, winner):
+    stich = stich_rule(played_cards=played_cards)
+    assert stich.player.name == winner
